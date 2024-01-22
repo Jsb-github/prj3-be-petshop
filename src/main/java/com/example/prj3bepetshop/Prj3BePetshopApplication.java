ed8afb90c3248bc7b3059d1d2e5e1224de2dc2ae
jsb <tjdqls2021@gmail.com>
2024-01-22 14:49:09 +0900

    최초 설정

---
+++ src/main/java/com/example/prj3bepetshop/Prj3BePetshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.prj3bepetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj3BePetshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prj3BePetshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prj3bepetshop/Prj3BePetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prj3bepetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj3BePetshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
